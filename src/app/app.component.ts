8bc9811e27e0a6107b48367e76b25bc36cc4c879
sto4711 <rstauffe@hsr.ch>
2018-08-12 12:19:03 +0200

    security vulnerability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'com-aho-rst-fee2018-projekt2-frontend';
+  title = 'Webshop';
 }
