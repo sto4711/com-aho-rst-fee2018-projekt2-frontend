e25d073afa62461167c0e9f4b1021d7c6e7eab64
sto4711 <rstauffe@hsr.ch>
2018-08-12 11:55:29 +0200

    new project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'com-aho-rst-fee2018-projekt2-frontend';
+}
