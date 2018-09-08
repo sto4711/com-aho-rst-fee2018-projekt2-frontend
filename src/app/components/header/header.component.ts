5998ac066ee5c3aecf0b2c88cb99c9ac32f9551f
Alain <alain@aholzhauser.ch>
2018-09-08 22:40:58 +0200

    UI optimizations

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,6 +14,7 @@ export class HeaderComponent implements OnInit {
   hamburger($event) {
     document.getElementsByClassName('bg-main-nav')[0].classList.toggle('expand');
     document.getElementsByClassName('main-nav')[0].classList.toggle('xs-nav');
+    document.getElementById('hamburger').classList.toggle('change');
 
   }
 }
