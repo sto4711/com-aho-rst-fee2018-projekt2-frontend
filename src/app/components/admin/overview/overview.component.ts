51e3c2700f119ff293e4f1d3dd834888cebad1de
Alain <alain@aholzhauser.ch>
2018-09-17 00:25:38 +0200

    - admin order

--- src/app/components/admin/overview/overview.component.ts
+++ src/app/components/admin/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Directive } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {OrderService} from '../../../services/order/order.service';
 import {Order} from '../../../services/order/order';
@@ -11,17 +11,16 @@ import {LangService} from '../../../services/lang-service/lang.service';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
-  public orders: Order;
-  public selectedState = 1;
+  public orders = [];
+  public selectedState = 'approved';
   public p: number = 1;
   private langSwitch: boolean;
   private panelOpenState = false;
-  private newOrderState: number = 1;
 
   public orderState = [
-    {value: 1, viewValue: 'In bearbeitung'},
-    {value: 2, viewValue: 'Versendet'},
-    {value: 3, viewValue: 'Annuliert'}
+    {value: 'approved', viewValue: 'In bearbeitung'},
+    {value: 'completed', viewValue: 'Versendet'},
+    {value: 'canceled', viewValue: 'Annuliert'}
   ];
   constructor(
     private route: ActivatedRoute,
@@ -46,12 +45,12 @@ export class OverviewComponent implements OnInit {
         this.orderService.getAll( )
           .subscribe(
             result => {
-              this.orders = result;
-              console.log(this.orders);
+             this.orders.push(result);
+
             }
           );
   }
   updateOrderState(orderState) {
-    this.newOrderState = orderState;
+    this.selectedState = orderState;
    }
 }
