ec8756e4c23238cdf87472ebdb120e9cc4be21ab
Roland Stauffer <rstauffe@hsr.ch>
2018-09-21 16:01:27 +0200

    - CheckoutResolverService added / clear Order / Interceptor cache (performance++)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule} from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { UserComponent } from './components/admin/user/user.component';
-import { HomeComponent } from './components/home/home.component';
-import { ArticleListingComponent } from './components/article-listing/article-listing.component';
-import { MyAccountComponent } from './components/my-account/my-account.component';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {UserComponent} from './components/admin/user/user.component';
+import {HomeComponent} from './components/home/home.component';
+import {ArticleListingComponent} from './components/article-listing/article-listing.component';
+import {MyAccountComponent} from './components/my-account/my-account.component';
 import {LoginService} from './services/login/login.service';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
-import { SearchComponent } from './components/search/search.component';
+import {ArticleDetailComponent} from './components/article-detail/article-detail.component';
+import {SearchComponent} from './components/search/search.component';
 import {TrimPipe} from './pipes/trim.pipe';
-import { ShoppingBasketComponent } from './components/shopping-basket/shopping-basket.component';
-import { ArticleComponent } from './components/admin/article/article.component';
-import { DialogConfirmComponent } from './components/commons/dialog/dialog-confirm/dialog-confirm.component';
+import {ShoppingBasketComponent} from './components/shopping-basket/shopping-basket.component';
+import {ArticleComponent} from './components/admin/article/article.component';
+import {DialogConfirmComponent} from './components/commons/dialog/dialog-confirm/dialog-confirm.component';
 import {AmountConverterPipe} from './pipes/currency.pipe';
-import { ArticleTemplateComponent } from './components/article-template/article-template.component';
+import {ArticleTemplateComponent} from './components/article-template/article-template.component';
 import {MatComponentsModule} from './mat-components/mat-components.module';
 import {MatCardModule} from '@angular/material/card';
-import { HeaderComponent } from './components/header/header.component';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { DialogConfirmDeleteComponent } from './components/commons/dialog/dialog-confirm-delete/dialog-confirm-delete.component';
-import { OrderDetailComponent } from './components/order-detail/order-detail.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
+import {HeaderComponent} from './components/header/header.component';
+import {TopBarComponent} from './components/top-bar/top-bar.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {DialogConfirmDeleteComponent} from './components/commons/dialog/dialog-confirm-delete/dialog-confirm-delete.component';
+import {OrderDetailComponent} from './components/order-detail/order-detail.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
 import {BreadcrumbComponent} from './components/breadcrumb/breadcrumb.component';
 import {DeTrimPipe} from './pipes/de-trim.pipe';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { LangSwitchComponent } from './components/lang-switch/lang-switch.component';
-import { ShoppingBasketListingComponent } from './components/shopping-basket-listing/shopping-basket-listing.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {LangSwitchComponent} from './components/lang-switch/lang-switch.component';
+import {ShoppingBasketListingComponent} from './components/shopping-basket-listing/shopping-basket-listing.component';
 import {LangService} from './services/lang-service/lang.service';
-import { OverviewComponent } from './components/admin/overview/overview.component';
-import {CheckoutResolverService} from "./services/route-resolver/checkout-resolver.service";
+import {OverviewComponent} from './components/admin/overview/overview.component';
+import {CheckoutResolverService} from "./services/checkout/checkout-resolver.service";
+import {RequestCacheService} from "./services/request-cache/request-cache.service";
+import {CacheInterceptor} from "./interceptor/cache-interceptor";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -65,12 +67,12 @@ export function HttpLoaderFactory(http: HttpClient) {
     SearchComponent,
     TrimPipe,
     DeTrimPipe,
-     AmountConverterPipe,
+    AmountConverterPipe,
     NotFoundComponent,
     LangSwitchComponent,
     ShoppingBasketListingComponent,
     OverviewComponent,
-   ],
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -90,9 +92,13 @@ export function HttpLoaderFactory(http: HttpClient) {
     })
   ],
   entryComponents: [
-    DialogConfirmComponent, DialogConfirmDeleteComponent ],
-  providers: [LoginService, LangService, CheckoutResolverService],
+    DialogConfirmComponent, DialogConfirmDeleteComponent],
+  providers: [LoginService, LangService, CheckoutResolverService,
+    RequestCacheService,
+    {provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
