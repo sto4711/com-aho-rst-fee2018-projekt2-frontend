e8463eb47a8be60e68e6b50f8bc5430aaf7c4c8a
Roland Stauffer <rstauffe@hsr.ch>
2018-10-01 11:27:26 +0200

    - refactoring

--- src/app/interceptors/articles-response-cache-interceptor.ts
+++ src/app/interceptors/articles-response-cache-interceptor.ts
@@ -2,26 +2,26 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpRequest, HttpResponse, HttpInterceptor, HttpHandler} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {tap} from 'rxjs/operators';
+import {ArticlesResponseCacheService} from "../services/articles-response-cache/articles-response-cache.service";
 
-import {RequestCacheService} from "../services/request-cache/request-cache.service";
 
 @Injectable()
-export class CacheInterceptor implements HttpInterceptor {
+export class ArticlesResponseCacheInterceptor implements HttpInterceptor {
 
-  constructor(private requestCacheService: RequestCacheService) {
+  constructor(private articlesResponseCacheService: ArticlesResponseCacheService) {
   }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.method === 'GET' && req.url.indexOf('articles') > 0) {//cache articles only
-      const cachedResponse = this.requestCacheService.get(req);
+      const cachedResponse = this.articlesResponseCacheService.get(req);
       if (cachedResponse) {
-        console.log('CacheInterceptor.intercept(), article response in cache, no backend call');
+        console.log('ArticlesResponseCacheInterceptor.intercept(), article response in cache, no backend call');
         return of(cachedResponse);
       } else {
         return next.handle(req).pipe(
           tap(event => {
             if (event instanceof HttpResponse) {
-              this.requestCacheService.put(req, event);
+              this.articlesResponseCacheService.put(req, event);
             }
           })
         );
