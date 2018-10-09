8dfbb2ab6f0b28b10cd4cd50efb21218e4af2eba
Roland Stauffer <rstauffe@hsr.ch>
2018-10-09 11:26:30 +0200

    - replacing all console.log entries by using Logger

--- src/app/services/articles/article.service.ts
+++ src/app/services/articles/article.service.ts
@@ -5,7 +5,7 @@ import {share, tap} from 'rxjs/operators';
 import {Article} from 'src/app/services/articles/article';
 import {ArticleRating} from "./article-rating";
 import {backendUrls} from "../../constants/backend-urls";
-
+import {Logger} from "../logger/logger";
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,7 @@ export class ArticleService {
   ) {
   }
 
-  searchArticles(term: string): Observable<Article[]> {
+  public searchArticles(term: string): Observable<Article[]> {
     if (term === null) {
       return of<Article[]>([]);
     }
@@ -27,35 +27,37 @@ export class ArticleService {
         headers: {'Content-Type': 'application/json'}
       }
     ).pipe(
-      share()  /* observable$ | async is used several times in template. To avoid similar backend calls */
+      share(), /* observable$ | async is used several times in template. To avoid similar backend calls */
+      tap(() => Logger.consoleLog(this.constructor.name, 'searchArticles', 'ok'))
     );
   }
 
-  getArticlesLatest(limit: number): Observable<Article[]> {
+  public getArticlesLatest(limit: number): Observable<Article[]> {
     return this.http
-      .get<Article[]>(  backendUrls.articlesLatest + '?limit=' + limit, {
+      .get<Article[]>(backendUrls.articlesLatest + '?limit=' + limit, {
           headers: {'Content-Type': 'application/json'}
         }
       ).pipe(
-        share()  /* observable$ | async is used several times in template. To avoid similar backend calls */
+        share(), /* observable$ | async is used several times in template. To avoid similar backend calls */
+        tap(() => Logger.consoleLog(this.constructor.name, 'getArticlesLatest', 'ok'))
       );
   }
 
-  getArticleDetails(articleQueryParameter: Article["articleQueryParameter"]): Observable<Article> {
-    return this.http.get<Article>(  backendUrls.articlesDetails + '?article=' + articleQueryParameter, {
+  public getArticleDetails(articleQueryParameter: Article["articleQueryParameter"]): Observable<Article> {
+    return this.http.get<Article>(backendUrls.articlesDetails + '?article=' + articleQueryParameter, {
         headers: {'Content-Type': 'application/json'}
       }
     ).pipe(
-      share()  /* observable$ | async is used several times in template. To avoid similar backend calls */
+      tap(() => Logger.consoleLog(this.constructor.name, 'getArticleDetails', 'ok'))
     );
   }
 
-  changeRating(articleRating: ArticleRating): Observable<Article> {
+  public changeRating(articleRating: ArticleRating): Observable<Article> {
     return this.http.patch<Article>(backendUrls.articlesDetails + 'change-rating', articleRating, {
         headers: {'Content-Type': 'application/json'}
       }
     ).pipe(
-      tap((article: Article) => console.log('changeRating ok'))
+      tap((article: Article) => Logger.consoleLog(this.constructor.name, 'changeRating', 'ok'))
     );
   }
 
