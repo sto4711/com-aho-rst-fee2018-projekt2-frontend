a89ab766512a45f36a233d6ff4645fee7321f8b3
Roland Stauffer <rstauffe@hsr.ch>
2018-10-18 14:44:35 +0200

    - log in added

--- src/app/components/login-info/login-info.component.ts
+++ src/app/components/login-info/login-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 import {UserService} from "../../services/user/user.service";
 import {Router} from "@angular/router";
 import {SnackBarService} from "../../services/commons/snack-bar/snack-bar.service";
@@ -8,19 +8,25 @@ import {SnackBarService} from "../../services/commons/snack-bar/snack-bar.servic
   templateUrl: './login-info.component.html',
   styleUrls: ['./login-info.component.scss']
 })
-export class LoginInfoComponent  {
+export class LoginInfoComponent {
   private static CODE_TRANSLATION_LOGOUT_SUCCESSFUL: string = 'LOGOUT-SUCCESSFUL';
 
   constructor(
     public userService: UserService
-    ,private snackBarService: SnackBarService
-  ,private router: Router
-  ) { }
+    , private snackBarService: SnackBarService
+    , private router: Router
+  ) {
+  }
 
-  public async onLogout() {
+  public async onLogOut() {
     await this.userService.signOut().toPromise();
     this.snackBarService.showInfo(LoginInfoComponent.CODE_TRANSLATION_LOGOUT_SUCCESSFUL);
     this.router.navigate(['home']).then();
   }
+
+  public async onGoToLogIn() {
+    this.router.navigate(['my-account']).then();
+  }
+
 
 }
