d8a322c58b43555781674e7825d53fc9697c6769
Alain <alain.holzhauser@mmp.htwchur.ch>
2018-11-13 11:25:42 +0100

    - price display change with '

--- src/app/pipes/currency.pipe.ts
+++ src/app/pipes/currency.pipe.ts
@@ -7,7 +7,7 @@ export class AmountConverterPipe implements PipeTransform {
 
   public transform(value: any | string): string {
     if (value)  {
-      return value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&');
+      return value.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1\'');
     } else {
       return value;
     }
