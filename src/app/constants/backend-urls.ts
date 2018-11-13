b007a1f9c4414ed97478b7ce692e06ca26b52bfb
Roland Stauffer <rstauffe@hsr.ch>
2018-11-13 09:25:36 +0100

    - backend URL changed

--- src/app/constants/backend-urls.ts
+++ src/app/constants/backend-urls.ts
@@ -3,16 +3,16 @@ import {environment} from '../../environments/environment';
 export const backendUrls: { public: string; articles: string; articlesLatest: string; articlesDetails: string; users: string;
  user: string; userDetails: string; shoppingBasket: string; order: string; orderDetails: string; orderAll: string; userOrders: string } = {
   public: environment.backendUrl,
-  articles: environment.backendUrl + 'webshop/articles/',
-  articlesLatest: environment.backendUrl + 'webshop/articles/latest',
-  articlesDetails: environment.backendUrl + 'webshop/article-details/',
-  users: environment.backendUrl + 'webshop/users/',
-  user: environment.backendUrl + 'webshop/user/',
-  userDetails: environment.backendUrl + 'webshop/user-details/',
-  shoppingBasket: environment.backendUrl + 'webshop/shopping-basket/',
-  order: environment.backendUrl + 'webshop/order/',
-  orderDetails: environment.backendUrl + 'webshop/order-details/',
-  orderAll: environment.backendUrl + 'webshop/order-all/',
-  userOrders: environment.backendUrl + 'webshop/user-orders/',
+  articles: environment.backendUrl + 'backend/articles/',
+  articlesLatest: environment.backendUrl + 'backend/articles/latest',
+  articlesDetails: environment.backendUrl + 'backend/article-details/',
+  users: environment.backendUrl + 'backend/users/',
+  user: environment.backendUrl + 'backend/user/',
+  userDetails: environment.backendUrl + 'backend/user-details/',
+  shoppingBasket: environment.backendUrl + 'backend/shopping-basket/',
+  order: environment.backendUrl + 'backend/order/',
+  orderDetails: environment.backendUrl + 'backend/order-details/',
+  orderAll: environment.backendUrl + 'backend/order-all/',
+  userOrders: environment.backendUrl + 'backend/user-orders/',
 };
 
