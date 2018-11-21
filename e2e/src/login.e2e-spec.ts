a638e71ed98c4f4179151bc058166332db5bdb1a
Roland Stauffer <rstauffe@hsr.ch>
2018-11-21 14:13:14 +0100

    - e2e user / shoppingBasket

--- e2e/src/login.e2e-spec.ts
+++ e2e/src/login.e2e-spec.ts
@@ -1,26 +1,50 @@
 import {MyAccount} from './login.po';
 import {browser} from 'protractor';
 import {Login} from '../../src/app/services/user/login';
+import {User} from '../../src/app/services/user/user';
 
 describe('myAccount login / create user', () => {
-  const loginDataNok: Login = new Login('wrong@email.com', '1234223322233test');
-  const loginDataOk: Login = new Login('alain@aholzhauser.ch', 'aho');
-  const names = ['Darth', 'Han', 'Luke', 'Leia'];
-  const firstnames = ['Vader', 'Solo', 'Organa'];
+  const loginNok: Login = new Login('wrong@email.com', '1234223322233test');
+  const loginOk: Login = new Login('alain@aholzhauser.ch', 'aho');
+  const usersToCreate: { name: string; firstname: string }[] = [
+    {name: 'Darth', firstname: 'Vader', },
+    {name: 'Han', firstname: 'Solo', },
+    {name: 'Luke', firstname: 'Skywalker', },
+    {name: 'Leia', firstname: 'Organa', },
+    {name: 'Indiana', firstname: 'Jones', },
+    {name: 'Pink', firstname: 'Panther', },
+  ];
 
   beforeEach(async () => {
     await MyAccount.navigateTo();
   });
 
   it('when login is fails - stay on page', async () => {
-    MyAccount.login(loginDataNok);
+    await MyAccount.login(loginNok);
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/my-account');
   });
 
-
-  it('when login is successful -  redicret to home page', async() => {
-    MyAccount.login(loginDataOk);
+  it('when login is successful -  redicret to home page', async () => {
+    await MyAccount.login(loginOk);
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/home');
   });
+
+  function createAndLogin(userToCreate: { name: string; firstname: string }) {
+    it('when user "' + userToCreate.name + ' ' +  userToCreate.firstname
+      + '" created (ignore, if already exists)  & login is successful -  redicret to home page', async () => {
+      const email = (userToCreate.name + '.' + userToCreate.firstname + '@starwars.com').toLowerCase().replace(' ', '');
+      const pwd = userToCreate.name.substr(0, 1) + userToCreate.firstname.substr(0, 2);
+      const user: User = new User(null, userToCreate.firstname, userToCreate.name, email, pwd, null);
+      await MyAccount.createUser(user);
+      await MyAccount.navigateTo();
+      await MyAccount.login(user);
+      await expect(await browser.getCurrentUrl()).toEqual('http://localhost:4200/home');
+    });
+  }
+
+  usersToCreate.forEach(async userToCreate => {
+    createAndLogin(userToCreate);
+  });
+
 
 });
--- e2e/src/login.po.ts
+++ e2e/src/login.po.ts
@@ -4,22 +4,28 @@ import {User} from '../../src/app/services/user/user';
 
 export class  MyAccount {
 
-  public static navigateTo() {
-    return browser.get('/my-account');
+  public static async navigateTo() {
+    return await browser.get('/my-account');
   }
 
-  public static login(login: Login) {
+  public static async login(login: Login) {
     element(by.css('[name="loginEmail"]')).sendKeys(login.email);
     element(by.css('[name="loginPassword"]')).sendKeys(login.pwd);
     element(by.css('[aria-label="anmelden"]')).click();
+    await browser.waitForAngular();
   }
 
-  public static createUser(user: User) {
+  public static async createUser(user: User)  {
+    element(by.css('[name="accountNewName"]')).clear();
     element(by.css('[name="accountNewName"]')).sendKeys(user.name);
+    element(by.css('[name="accountNewFirstname"]')).clear();
     element(by.css('[name="accountNewFirstname"]')).sendKeys(user.firstname);
+    element(by.css('[name="accountNewEmail"]')).clear();
     element(by.css('[name="accountNewEmail"]')).sendKeys(user.email);
+    element(by.css('[name="accountNewPassword"]')).clear();
     element(by.css('[name="accountNewPassword"]')).sendKeys(user.pwd);
     element(by.css('[aria-label="Konto erstellen"]')).click();
+    await browser.waitForAngular();
   }
 
 }
