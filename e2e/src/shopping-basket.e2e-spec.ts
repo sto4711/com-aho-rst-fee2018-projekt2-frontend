3735da24decc9781669e76df0d68ac2caecbe07f
Alain <alain@aholzhauser.ch>
2018-11-25 17:58:25 +0100

    - e2e shoppingBasket

--- e2e/src/shopping-basket.e2e-spec.ts
+++ e2e/src/shopping-basket.e2e-spec.ts
@@ -4,12 +4,15 @@ import {ShoppingBasketPo} from './shopping-basket.po';
 describe('shopping basket testing', () => {
 
   beforeEach(async () => {
-    await ShoppingBasketPo.navigateToArticle();
   });
 
   it('navigate to article', async () => {
-     await expect(browser.getCurrentUrl()).toEqual('http://localhost:4300/article-detail?article=Superbike-E-Bike-Modell-1');
-     await ShoppingBasketPo.addArticle();
+    await ShoppingBasketPo.navigateToArticle();
+    await expect(browser.getCurrentUrl()).toEqual('http://localhost:4300/article-detail?article=Superbike-E-Bike-Modell-1');
+
+  });
+  it('add article', async () => {
+    await ShoppingBasketPo.addArticle();
 
   });
 
@@ -21,7 +24,22 @@ describe('shopping basket testing', () => {
   it('navigate to shopping basket', async () => {
     await ShoppingBasketPo.navigateToShoppingBasket();
     await expect(browser.getCurrentUrl()).toEqual('http://localhost:4300/shopping-basket');
+  });
 
+  it('navigate to login', async () => {
+    await ShoppingBasketPo.navigateToLogin();
+    await expect(browser.getCurrentUrl()).toEqual('http://localhost:4300/my-account');
+
+  });
+
+  it('login user', async () => {
+    await ShoppingBasketPo.loginUser(ShoppingBasketPo.loginOk);
+    await expect(browser.getCurrentUrl()).toEqual('http://localhost:4300/checkout');
+  });
+
+  it('write customer data', async () => {
+    await ShoppingBasketPo.writeCustomerData(ShoppingBasketPo.customerData);
+    await expect(browser.getCurrentUrl()).toEqual('http://localhost:4300/checkout');
   });
 
 });
--- e2e/src/shopping-basket.po.ts
+++ e2e/src/shopping-basket.po.ts
@@ -1,9 +1,15 @@
 import { browser, by, element } from 'protractor';
 import {ShoppingBasketService} from '../../src/app/services/shopping-basket/shopping-basket.service';
+import {Login} from '../../src/app/services/user/login';
 
 
 export class  ShoppingBasketPo {
   public static shoppingBasketService: ShoppingBasketService;
+  public static loginOk: Login = new Login('alain@aholzhauser.ch', 'aho');
+  public static customerData:  { street: string; plz: string; city: string; phone: string }[] = [
+    {street: 'Wiesenstrasse 2', plz: '8020', city: 'Zürich', phone: '+41 78 546 52 32' },
+  ];
+
 
   public static async navigateToArticle() {
     return await browser.get('/article-detail?article=Superbike-E-Bike-Modell-1');
@@ -15,6 +21,12 @@ export class  ShoppingBasketPo {
 
   }
 
+  public static async navigateToLogin() {
+    element(by.css('[aria-label="checkout"]')).click();
+    await browser.waitForAngular();
+
+  }
+
   public static async addArticle() {
     element(by.css('[aria-label="In den Warenkorb"]')).click();
   }
@@ -22,5 +34,30 @@ export class  ShoppingBasketPo {
   public static async getShoppingBasketLength() {
     const amount = element(by.id('mat-badge-content-0')).getText();
     return amount;
+  }
+
+  public static async loginUser(login: Login) {
+    element(by.css('[name="loginEmail"]')).sendKeys(login.email);
+    element(by.css('[name="loginPassword"]')).sendKeys(login.pwd);
+    element(by.css('[aria-label="anmelden"]')).click();
+    await browser.waitForAngular();
+  }
+  public static async writeCustomerData(customerData: object) {
+    element(by.css('[aria-label="street"]')).sendKeys(customerData[0].street);
+    element(by.css('[aria-label="post code"]')).sendKeys(customerData[0].plz);
+    element(by.css('[aria-label="city"]')).sendKeys(customerData[0].city);
+    element.all(by.className('matStepperNext')).get(0).click();
+    browser.sleep(1000);
+    element(by.css('[aria-label="phone"]')).sendKeys(customerData[0].phone);
+    element.all(by.className('matStepperNext')).get(1).click();
+    browser.sleep(1000);
+    element(by.css('[aria-label="PostPac Priority"]')).click();
+    element.all(by.className('matStepperNext')).get(2).click();
+    browser.sleep(1000);
+    element(by.css('[aria-label="Paypal"]')).click();
+    element.all(by.className('matStepperNext')).get(3).click();
+    browser.sleep(1000);
+    element(by.css('[aria-label="Jetzt bestellen"]')).click();
+    await browser.waitForAngular();
   }
 }
