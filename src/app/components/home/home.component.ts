302b8876e5551567b3b4bd51e180176cc98437ad
Alain Holzhauser <alain.holzhauser@mmp.htwchur.ch>
2018-12-05 21:14:00 +0100

    - checkout btns

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,34 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component } from '@angular/core';
 
 import {ArticleService} from 'src/app/services/articles/article.service';
 import {Article} from 'src/app/services/articles/article';
 import {Observable} from 'rxjs';
-import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent  implements OnInit  {
+export class HomeComponent  {
   public articles$: Observable<Article[]>;
-  private currentURL:
 
   constructor(
     private articleService: ArticleService,
-    private route: ActivatedRoute,
-
   ) {
     this.articles$ = this.articleService.getArticlesLatest(4);
   }
 
-  public ngOnInit(): void {
-    this.route.paramMap
-      .subscribe( () => {
-        this.currentURL =
-        if (this.queryParams[/'home'] && this.route.snapshot.queryParams['article'] !== '') {
-
-        }
-      });
-  }
 }
